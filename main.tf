a7579e627885171cc0a1b4a50294afdb410a8f2b
José Eduardo Chapa González <edchapa@Mac-mini-de-Eduardo.local>
2026-02-14 13:07:31 -0600

    Added missing code and corrected config

--- main.tf
+++ main.tf
@@ -1,3 +1,25 @@
+provider "kubernetes" {
+  cluster_ca_certificate = base64decode(var.kubernetes_cluster_cert_data)
+  host                   = var.kubernetes_cluster_endpoint
+  exec {
+    api_version = "client.authentication.k8s.io/v1alpha1"
+    command     = "aws-iam-authenticator"
+    args        = ["token", "-i", "${var.kubernetes_cluster_name}"]
+  }
+}
+
+provider "helm" {
+  kubernetes = {
+    cluster_ca_certificate = base64decode(var.kubernetes_cluster_cert_data)
+    host                   = var.kubernetes_cluster_endpoint
+    exec = {
+      api_version = "client.authentication.k8s.io/v1alpha1"
+      command     = "aws-iam-authenticator"
+      args        = ["token", "-i", "${var.kubernetes_cluster_name}"]
+    }
+  }
+}
+
 resource "kubernetes_namespace_v1" "example" {
   metadata {
     name = "argo"
