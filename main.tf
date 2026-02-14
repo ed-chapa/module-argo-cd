616d8414972288a4d892b92d281384c682e40ed0
José Eduardo Chapa González <edchapa@Mac-mini-de-Eduardo.local>
2026-02-14 12:46:42 -0600

    ArgoCD module init

---
+++ main.tf
@@ -0,0 +1,12 @@
+resource "kubernetes_namespace_v1" "example" {
+  metadata {
+    name = "argo"
+  }
+}
+
+resource "helm_release" "argocd" {
+  name       = "msur"
+  chart      = "argo-cd"
+  repository = "https://argoproj.github.io/argo-helm"
+  namespace  = "argo"
+}
